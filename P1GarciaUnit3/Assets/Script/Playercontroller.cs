df23d86e281316b83f1f58205bc8fa5211bc213a
749759-Cloud <749759@student.dusd.net>
2026-03-25 11:45:13 -0700

    Moving left and spawn manager

--- P1GarciaUnit3/Assets/Script/Playercontroller.cs
+++ P1GarciaUnit3/Assets/Script/Playercontroller.cs
@@ -1,16 +1,32 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float jumpForce = 10f;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) &&isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
